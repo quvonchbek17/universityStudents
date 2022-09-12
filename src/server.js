a541fb3fb36876ed439c029285e1c106914ae48f
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2022-09-12 22:20:13 +0500

    b

--- src/server.js
+++ src/server.js
@@ -16,8 +16,11 @@ const cors = require('cors')
 // )
 
 var corsOptions = {
-    origin: 'http://207.154.246.125:3000',
-}
+    "origin": "http://localhost:3000",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }
 
 
 dotenv.config()
