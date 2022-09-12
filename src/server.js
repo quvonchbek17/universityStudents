96994e0e6c438ac0e05e8da6a2bbab7e9acc0b68
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2022-09-12 20:27:45 +0500

    security

--- src/server.js
+++ src/server.js
@@ -2,14 +2,12 @@ const express = require('express')
 const dotenv = require('dotenv')
 const app = express()
 const router = require('./modules')
-const cors = require('cors')
 
-const whitelist = ['http://localhost:3000']
 
 app.use('/*', (req, res) => {
-    console.log(req.headers.host);
-    if(req.headers.host != 'localhost:3000'){
+    if(req.headers.origin != 'http://localhost:3000'){
         res.sendStatus(404)
+        return
     }
    }
 )
