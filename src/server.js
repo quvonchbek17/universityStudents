5c505a73ded0470547dc9bd60f44caaaf85aaa5f
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2022-09-12 21:25:29 +0500

    e

--- src/server.js
+++ src/server.js
@@ -5,17 +5,13 @@ const router = require('./modules')
 const cors = require('cors')
 
 
-app.use('/*', (req, res) => {
-    if(req.headers.origin != 'http://localhost:3000'){
-        res.sendStatus(404)
-        return
-    }
-   }
-)
+var corsOptions = {
+    origin: 'http://localhost:3000',
+}
 
 dotenv.config()
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(router)
 app.use('/*', (req, res) => res.sendStatus(404))
 const port =  8080
