978badad81b7bc255adbf8209c3a368bbd0c0d2c
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2022-09-12 21:34:57 +0500

    t

--- src/modules/directions/directions.js
+++ src/modules/directions/directions.js
@@ -46,6 +46,7 @@ module.exports = {
         });
       }
     } catch (err) {
+
       res.sendStatus(500);
     }
   },
--- src/server.js
+++ src/server.js
@@ -5,9 +5,20 @@ const router = require('./modules')
 const cors = require('cors')
 
 
+// app.use('/*', (req, res) => {
+//     console.log(req.headers.origin);
+//     console.log(req.headers.origin != 'http://localhost:3000');
+//     if(req.headers.origin != 'http://localhost:3000'){
+//         res.sendStatus(404)
+//         return
+//     }
+//    }
+// )
+
 var corsOptions = {
     origin: 'http://localhost:3000',
 }
+
 
 dotenv.config()
 app.use(express.json())
