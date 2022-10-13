73383d2480cc11fd8ee4249f22b4b17fcef75c11
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2022-10-13 15:31:26 +0500

    loadtest

--- src/server.js
+++ src/server.js
@@ -3,8 +3,16 @@ const dotenv = require('dotenv')
 const app = express()
 const router = require('./modules')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000,
+	max: 10000,
+	standardHeaders: true,
+	legacyHeaders: false,
+})
 
+app.use(limiter)
 dotenv.config()
 app.use(express.json())
 app.use(cors())
