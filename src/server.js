0d7a282df81d3980e57f08fa151d873af6d9b8ff
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2022-10-13 16:06:58 +0500

    loadtest updated

--- src/server.js
+++ src/server.js
@@ -7,7 +7,9 @@ const rateLimit = require('express-rate-limit')
 
 const limiter = rateLimit({
 	windowMs: 10 * 60 * 1000,
-	max: 10000
+	max: 10000,
+	standardHeaders: true,
+	legacyHeaders: false,
 })
 
 app.use(limiter)
